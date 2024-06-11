14fc6493233532638283696a58ae2fc2fce2244c
dirhamtriyadi <dirhamtriyadi@gmail.com>
2024-06-11 12:49:06 +0700

    style: clean admin page

--- resources/views/templates/sidebar.blade.php
+++ resources/views/templates/sidebar.blade.php
@@ -10,10 +10,10 @@
     <div class="sidebar">
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            {{-- <div class="image">
-                <img src="{{ asset('adminlte') }}/dist/img/user2-160x160.jpg" class="img-circle elevation-2"
+            <div class="image">
+                <img src="{{ asset('adminlte') }}/dist/img/avatar5.png" class="img-circle elevation-2"
                     alt="User Image">
-            </div> --}}
+            </div>
             <div class="info">
                 <a href="#" class="d-block">{{ auth()->user()->name }}</a>
             </div>
